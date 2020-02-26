81279786020e83bc79da347274daff6a0a91539f
pqrstty <1015405927@qq.com>
2020-02-25 17:02:56 -0800

    Add files via upload

---
+++ Doatz/python/VotingSys.py
@@ -0,0 +1,214 @@
+import json
+import threading
+from time import time
+#from time import clock
+from urllib.parse import urlparse
+from uuid import uuid4
+
+# create subprocess
+import subprocess
+
+# create timer
+import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+import requests
+from flask import Flask, jsonify, request
+
+
+
+class VotingSys:
+    def __init__(self):
+        self.voteChain = []
+        #self.startMiningTimer()        
+        self.port = 5000 #use 5000 as defort port
+        #self.newChainPort = self.port
+
+    #Triggered by the message from sponsor
+    def createVoteChain(self, voteIntro, candidate, emailList, startTime, endTime):
+
+        print("===============================================")
+        print(time())    
+        print("============TESTING CREATE CHAIN===============") 
+        print("===============================================")
+
+        #Set the basic information of a new chain (port)
+        
+        newChain = {
+            'index': len(self.voteChain) + 1,
+            'timestamp': time(),
+            'voteIntro': voteIntro,
+            'candidate': candidate,
+            'emailList': emailList,
+            'startTime': startTime,
+            'endTime': endTime,
+            'port': str(self.calculatePortFromInfo( voteIntro , len(self.voteChain)+1 )),
+            'password': str(uuid4()).replace('-', '')[-6:]
+        }
+
+        #launch a new blockchain as a subprocess                
+        loader = subprocess.Popen(["pipenv","run","python","blockchain.py","-p",newChain['port'],"-k",newChain['password']])
+        # I wonder if we can do sth. with loader
+       
+        #add the chain to self.voteChain
+        self.voteChain.append(newChain)
+
+        #start initialize the vote
+        #save the Vote Info to the chain (by send sth. like a transfer)
+        self.initNewBlockChain(newChain['index'])
+        
+        #then set a timer to start the vote in time
+        self.votingStartTimer(newChain['index'],startTime)
+
+        #add a new timer to stop the vote
+        self.votingTimeCountDown(newChain['index'],endTime)
+    
+    def calculatePortFromInfo(self, voteIntro, voteIndex):
+        #set the port (should be crypto before sending email, now only for test)
+        return self.port + voteIndex
+
+    # probably this should be done by BlockChain
+    def sendAttenderEmail(self, emailList):
+        #send everyone a email containing the PrivateKey, ID, IP & Port
+        return
+
+    def initNewBlockChain(self, voteIndex):
+        #send message to BlockChain to init the forst several block and create crypto key
+
+
+
+        return
+
+
+    def autoMining(self):
+        for k in self.voteChain:
+            #if time()>k['startTime'] and time()<k['endTime']:    #if k is not finished yet
+            if True:
+                r = requests.get("http://localhost:"+k['port']+"/mine")
+                #print(r.json())
+
+    def startMiningTimer(self):
+        #Create a timer, every 10 seconds, traverse through the voteChain
+        scheduler = BlockingScheduler()
+        scheduler.add_job(self.autoMining, 'interval', seconds=10, id='test_job1')
+        scheduler.start()
+        
+    
+    def votingStartTimer(self, voteIndex, startTime):
+        # set a timer, when vote start, tell BlockChain to send email to attender
+
+        # post : /sendStartEmail ["key":"xxx"]
+
+
+        return
+
+    def votingTimeCountDown(self, voteIndex, endTime):
+        # set a timer, when time end, send specified Post to BlockChain
+
+        # post: /voteTimeEnd ["key":"xxx"]
+        # the key here should be set later in the project
+
+        return
+
+
+    #followed func will be triggered by a message from BlockChain
+    def finishVote(self, voteIndex):
+        #1 check the vote time is ended
+        if self.voteChain[voteIndex]['endTime']<time():
+            return {"warning":"VOTE have NOT finished yet!!!"}
+
+        #2 check the chain to see if it did recieved the special message
+
+        # I guess this can be done by countVote()
+        #if self.checkMineOut(voteIndex) :
+        #    return {"warning":"VOTE have NOT finished yet!!!"}
+
+        #3 count the vote and printout result
+        xxx = self.countVote(voteIndex)
+
+        #if xxx['finish'] == 0 :
+        #    return {"warning":"VOTE have NOT finished yet!!!"}
+        
+        #4 set a timer to stop the blockChain after several days
+        self.createFinishedVoteTimer(voteIndex)
+
+        return xxx
+
+    def countVote(self, voteIndex):
+        #countTheVote and PrintOut(and other things)
+
+
+        return 
+
+    def createFinishedVoteTimer(self, voteIndex):
+        # set a timer, download all the info of that chain
+        #then, shutdown that block chain, and delete the vote in self.voteChain
+
+
+
+        return
+    
+    
+
+app = Flask(__name__)
+# Instantiate the VotingSystem
+votingSystem = VotingSys()
+
+@app.route('/hello', methods=['GET'])
+def helloWorld():
+    print("hello world")
+    return jsonify({"message":"hello world"}), 201
+
+@app.route('/createVote', methods=['POST'])
+def createVote():
+    values = request.get_json()
+    # Check that the required fields are in the POST'ed data
+    required = ['voteIntro', 'candidate', 'emailList', 'startTime', 'endTime']
+    # mayby deal with Possible fields like 'voteName', 'briefIntro' later...
+
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    #Create the chain
+    xxx = votingSystem.createVoteChain(values['voteIntro'], values['candidate'], values['emailList'], values['startTime'], values['endTime'])
+
+    ###################
+    ## design sth. to return 
+    ###################
+
+    return jsonify(xxx), 201
+
+@app.route('/voteFinish', methods=['POST'])
+def mineOutSpecialInfo():
+    values = request.get_json()
+    # Check that the required fields are in the POST'ed data
+    required = ['voteIndex']
+
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    xxx = votingSystem.finishVote(values['voteIndex'])
+
+    ###################
+    ## design sth. to return 
+    ###################
+
+    return jsonify(xxx), 202
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    votingSystem.port = port
+
+    t1 = threading.Thread(target=app.run, args=('0.0.0.0',port))
+    t2 = threading.Thread(target=votingSystem.startMiningTimer)
+    t1.start()
+    t2.start()
+    #app.run(host='0.0.0.0', port=port)
+
+
--- Doatz/python/blockchain.py
+++ Doatz/python/blockchain.py
@@ -1,16 +1,22 @@
 import hashlib
 import json
 from time import time
+#from time import clock
 from urllib.parse import urlparse
 from uuid import uuid4
 
 import requests
+from flask import Flask, jsonify, request
+
 
 class Blockchain:
     def __init__(self):
         self.current_transactions = []
         self.chain = []
         self.nodes = set()
+        self.port = "5000"
+        self.password = ""
+        #self.voteInfo = {}
 
         # Create the genesis block
         self.new_block(previous_hash='1', proof=100)
@@ -18,15 +24,24 @@ class Blockchain:
     def register_node(self, address):
         """
         Add a new node to the list of nodes
+
         :param address: Address of node. Eg. 'http://192.168.0.5:5000'
         """
 
         parsed_url = urlparse(address)
-        self.nodes.add(parsed_url.netloc)
+        if parsed_url.netloc:
+            self.nodes.add(parsed_url.netloc)
+        elif parsed_url.path:
+            # Accepts an URL without scheme like '192.168.0.5:5000'.
+            self.nodes.add(parsed_url.path)
+        else:
+            raise ValueError('Invalid URL')
+
 
     def valid_chain(self, chain):
         """
         Determine if a given blockchain is valid
+
         :param chain: A blockchain
         :return: True if valid, False if not
         """
@@ -40,11 +55,12 @@ class Blockchain:
             print(f'{block}')
             print("\n-----------\n")
             # Check that the hash of the block is correct
-            if block['previous_hash'] != self.hash(last_block):
+            last_block_hash = self.hash(last_block)
+            if block['previous_hash'] != last_block_hash:
                 return False
 
             # Check that the Proof of Work is correct
-            if not self.valid_proof(last_block['proof'], block['proof']):
+            if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
                 return False
 
             last_block = block
@@ -56,6 +72,7 @@ class Blockchain:
         """
         This is our consensus algorithm, it resolves conflicts
         by replacing our chain with the longest one in the network.
+
         :return: True if our chain was replaced, False if not
         """
 
@@ -88,6 +105,7 @@ class Blockchain:
     def new_block(self, proof, previous_hash):
         """
         Create a new Block in the Blockchain
+
         :param proof: The proof given by the Proof of Work algorithm
         :param previous_hash: Hash of previous Block
         :return: New Block
@@ -110,6 +128,7 @@ class Blockchain:
     def new_transaction(self, sender, recipient, amount):
         """
         Creates a new transaction to go into the next mined Block
+
         :param sender: Address of the Sender
         :param recipient: Address of the Recipient
         :param amount: Amount
@@ -123,6 +142,19 @@ class Blockchain:
 
         return self.last_block['index'] + 1
 
+    def sendAttenderEmail(self, emailList, port):
+
+        return
+
+    def setVoteInitInfo(self, voteInfo):
+        #Init the Voting info
+        #1 add a new block that include intro & candidate
+
+
+        #2 send email to all attender (with cryptokey) and save the list of key
+        self.sendAttenderEmail(voteInfo.get('emailList'),voteInfo.get('port'))
+
+
     @property
     def last_block(self):
         return self.chain[-1]
@@ -131,6 +163,7 @@ class Blockchain:
     def hash(block):
         """
         Creates a SHA-256 hash of a Block
+
         :param block: Block
         """
 
@@ -138,28 +171,194 @@ class Blockchain:
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-    def proof_of_work(self, last_proof):
+    def proof_of_work(self, last_block):
         """
         Simple Proof of Work Algorithm:
-         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
-         - p is the previous proof, and p' is the new proof
+
+         - Find a number p' such that hash(pp') contains leading 4 zeroes
+         - Where p is the previous proof, and p' is the new proof
+         
+        :param last_block: <dict> last Block
+        :return: <int>
         """
 
+        last_proof = last_block['proof']
+        last_hash = self.hash(last_block)
+        
         proof = 0
-        while self.valid_proof(last_proof, proof) is False:
+        while self.valid_proof(last_proof, proof, last_hash) is False:
             proof += 1
+
+        #print(proof)
 
         return proof
 
     @staticmethod
-    def valid_proof(last_proof, proof):
+    def valid_proof(last_proof, proof, last_hash):
         """
         Validates the Proof
-        :param last_proof: Previous Proof
-        :param proof: Current Proof
-        :return: True if correct, False if not.
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :param last_hash: <str> The hash of the Previous Block
+        :return: <bool> True if correct, False if not.
+
         """
 
-        guess = f'{last_proof}{proof}'.encode()
+        guess = f'{last_proof}{proof}{last_hash}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+    
+
+
+# Instantiate the Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+
+    #time_start=clock()
+    
+    proof = blockchain.proof_of_work(last_block)
+    
+    #time_end=clock()
+    #print('time cost:',time_end-time_start)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
+@app.route('/initVote', methods=['POST'])
+def initVoteMessage():
+    values = request.get_json()
+
+    required = ['timestamp', 'voteIntro', 'candidate', 'emailList', 'startTime', 'endTime', 'port', 'password']
+    # mayby deal with Possible fields like 'voteName', 'briefIntro' later...
+
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    if blockchain.port != values.get('port'):
+        return "Port Error: Send to the wrong port", 400
+
+    if blockchain.password != values.get('password'):
+        return "Error: Your password does not fit to this chain", 401
+
+    print(values)
+
+    blockchain.setVoteInitInfo(values)
+
+    #for node in nodes:
+    #   blockchain.register_node(node)
+
+    response = {
+        'message': 'password Committed',
+        #'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    parser.add_argument('-k', '--key', default='votingKey', type=str, help='controling Password')
+    args = parser.parse_args()
+    blockchain.port = args.port
+    blockchain.password = args.key
+
+    print("=============NEW CHAIN CREAT============")
+    print("port: ",blockchain.port)
+    print("password: ",blockchain.password)
+
+    app.run(host='0.0.0.0', port=blockchain.port)
