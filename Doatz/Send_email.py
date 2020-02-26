aadf89099793a0faec8c16b7045690ef87a5c83a
GODV <wumuting@umich.edu>
2020-02-25 16:19:36 -0800

    Update Send_email.py

--- Doatz/Send_email.py
+++ Doatz/Send_email.py
@@ -2,6 +2,10 @@ from email.mime.text import MIMEText
 from email.header import Header
 from smtplib import SMTP_SSL
 
+
+#sendAttenderEmail(self, emailList)
+emailList = ['1251563748@qq.com', '1713363421@qq.com']
+
 #qq邮箱smtp服务器
 host_server = 'smtp.qq.com'
 #sender_qq为发件人的qq号码
@@ -10,22 +14,25 @@ sender_qq = '1713363421'
 pwd = 'choooqcngrlyfbbg'
 #发件人的邮箱
 sender_qq_mail = '1713363421@qq.com'
+
 #收件人邮箱
-receiver = 'wumutingtingting@gmail.com'
+#receiver = 'wumutingtingting@gmail.com'
 #邮件的正文内容
-mail_content = '你好，我是来自知乎的[邓旭东HIT] ，现在在进行一项用python登录qq邮箱发邮件的测试'
+mail_content = '你好，我是来自知乎的[吴牧霆MIT] ，现在在进行一项用python登录qq邮箱发邮件的测试'
 #邮件标题
-mail_title = '邓旭东HIT 的邮件'
+mail_title = '吴牧霆MIT 的邮件'
 
-smtp = SMTP_SSL(host_server)
-#set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
-smtp.set_debuglevel(1)
-smtp.ehlo(host_server)
-smtp.login(sender_qq, pwd)
-
-msg = MIMEText(mail_content, "plain", 'utf-8')
-msg["Subject"] = Header(mail_title, 'utf-8')
-msg["From"] = sender_qq_mail
-msg["To"] = receiver
-smtp.sendmail(sender_qq_mail, receiver, msg.as_string())
-smtp.quit()
+for email in emailList:
+    smtp = SMTP_SSL(host_server)
+    #set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
+    smtp.set_debuglevel(1)
+    smtp.ehlo(host_server)
+    smtp.login(sender_qq, pwd)
+    
+    receiver = email
+    msg = MIMEText(mail_content, "plain", 'utf-8')
+    msg["Subject"] = Header(mail_title, 'utf-8')
+    msg["From"] = sender_qq_mail
+    msg["To"] = receiver
+    smtp.sendmail(sender_qq_mail, receiver, msg.as_string())
+    smtp.quit()
